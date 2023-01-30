2b42f84ea4432d7afc602431ee5f90983aabbed4
jaewoo <comjayoncloud@gmail.com>
2023-01-30 18:29:13 +0900

    그리디 : 카드정렬하기(heapq)2

--- BAEKJOON/그리디알고리즘/카드정렬하기.py
+++ BAEKJOON/그리디알고리즘/카드정렬하기.py
@@ -1,9 +1,10 @@
+import sys
 from heapq import heappush, heappop
 
-n = int(input())
+n = int(sys.stdin.readline())
 card = []
 for _ in range(n):
-    heappush(card, int(input()))
+    heappush(card, int(sys.stdin.readline()))
 
 count = 0
 for i in range(n-1):
@@ -17,4 +18,7 @@ print(count)
 """
 heapq 없이 해볼려햇는데 너무 복잡..
 heapq : 2진트리. 작은수 (루트)
+파이썬에서 input은 메모리를 많이 잡아먹어서 시간초과가 뜬다. 고로 input대신
+sys를 쓰자.
+
 """
