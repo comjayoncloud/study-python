197ccac77ab2f826b370894315887b65c32e7217
jaewoo <comjayoncloud@gmail.com>
2023-01-31 19:42:26 +0900

    그리디 : 세탁소사장동혁

---
+++ BAEKJOON/그리디알고리즘/세탁소사장동혁.py
@@ -0,0 +1,13 @@
+n = int(input())
+moneys = [25,10,5,1]
+count = [0]*4
+
+
+for i in range(n):
+    C = int(input())
+
+    for j in range(len(moneys)):
+        count[j] = C// moneys[j]
+        C = C%moneys[j]
+
+    print(' '.join(map(str, count)))
