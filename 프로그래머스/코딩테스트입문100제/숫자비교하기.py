8a50cd3ebc61c2518eb75f39ec19a4a9daac9385
jaewoo <comjayoncloud@gmail.com>
2023-02-03 18:11:06 +0900

    테스트100 : 숫자비교하기

---
+++ 프로그래머스/코딩테스트입문100제/숫자비교하기.py
@@ -0,0 +1,16 @@
+def solution(num1, num2):
+    answer = 0
+    if num1==num2:
+        answer = 1
+    else :
+        answer = -1
+    return answer
+
+
+"""
+다른사람
+
+def solution(num1, num2):
+    return 1 if num1==num2 else -1
+    
+"""
