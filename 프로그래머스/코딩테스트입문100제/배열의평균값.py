b14b2a65bf567160fbef3cea90503b48668a2831
jaewoo <comjayoncloud@gmail.com>
2023-02-06 17:39:41 +0900

    코테 100 : 배열의 평균값

---
+++ 프로그래머스/코딩테스트입문100제/배열의평균값.py
@@ -0,0 +1,17 @@
+def solution(numbers):
+    
+    return sum(numbers)/len(numbers)
+
+
+"""
+다른사람풀이:
+    
+import numpy as np
+def solution(numbers):
+    return np.mean(numbers)
+"""
+
+"""
+느낀점 :
+    넘파이를 사용하는 경우도 많은거 같다 . numpy (라이브러리)에 대해 좀 보도록하자
+"""
