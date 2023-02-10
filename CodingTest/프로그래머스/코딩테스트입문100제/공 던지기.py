d9c6f6b7874d856e8ccbbc861e309b6dda1d1d4e
jaewoo <comjayoncloud@gmail.com>
2023-02-10 14:58:54 +0900

    코테 100 : 공 던지기

--- CodingTest/프로그래머스/코딩테스트입문100제/공 던지기.py
+++ CodingTest/프로그래머스/코딩테스트입문100제/공 던지기.py
@@ -1,4 +1,8 @@
-def solution(numbers, k):
+
+""" 
+답은 맞는데 런타임에러..
+
+# def solution(numbers, k):
 #     if len(numbers)%2 ==0:
 #         numbers = numbers[0::2]
 #     else:
@@ -16,3 +20,18 @@ def solution(numbers, k):
 # solution([1, 2, 3, 4],2) 
 # solution([1, 2, 3,4,5,6],5)
 # solution([1, 2, 3] , 2)
+"""
+
+def solution(numbers,k):
+    players = numbers[0::2] if len(numbers)%2==0 else numbers[0::2]+numbers[1::2]
+    return players[k%len(players)-1]
+
+
+
+"""
+다른사람코드
+
+def solution(numbers, k):
+    return numbers[2 * (k - 1) % len(numbers)]
+"""
+
