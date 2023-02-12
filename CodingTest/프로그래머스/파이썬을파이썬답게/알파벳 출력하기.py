4688c5d2492be6d06ac187cde203b3a58f7971d8
jaewoo <comjayoncloud@gmail.com>
2023-02-12 15:12:42 +0900

    파이썬답게 : 문자열출력하기 (import string)

--- CodingTest/프로그래머스/파이썬을파이썬답게/알파벳 출력하기.py
+++ CodingTest/프로그래머스/파이썬을파이썬답게/알파벳 출력하기.py
@@ -1,7 +1,18 @@
 num = int(input().strip())
-print(num)
-answer ="ABCD...(중간생략)..XYZ"
+answer ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 if num == 0:
     print(answer.lower())
 elif num == 1:
     print(answer.upper())
+
+# str의 메소드 lower,upper
+
+
+"""
+import string
+string.ascii_lowercase # 소문자 abcdefghijklmnopqrstuvwxyz
+string.ascii_uppercase # 대문자 ABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.ascii_letters # 대소문자 모두 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.digits # 숫자 0123456789
+"""
