e40496cdd2152e5120fb5f25da3c1f3990212b93
jaewoo <comjayoncloud@gmail.com>
2023-02-16 21:57:33 +0900

    코테 100 : 팩토리얼

--- CodingTest/프로그래머스/코딩테스트입문100제 - 레벨0/팩토리얼.py
+++ CodingTest/프로그래머스/코딩테스트입문100제 - 레벨0/팩토리얼.py
@@ -1,6 +1,29 @@
+from math import factorial
+
 def solution(n):
     answer = 0
+    while True:
+        answer += 1
+        b = factorial(answer)
+        if b == n:
+            print(answer)
+            break
+        if b > n:
+            answer -=1
+            break
     return answer
 
 
+solution(3628800)
 solution(7)
+
+"""
+다른사람코드
+    from math import factorial
+
+    def solution(n):
+        k = 10
+        while n < factorial(k):
+            k -= 1
+        return k
+"""
