764178804a6348aed5058006944328ebfd285283
jaewoo <comjayoncloud@gmail.com>
2023-02-16 16:15:19 +0900

    코테 100 : 암호해독

---
+++ CodingTest/프로그래머스/코딩테스트입문100제 - 레벨0/암호해독.py
@@ -0,0 +1,11 @@
+def solution(cipher, code):
+    answer = ''
+
+    # for p1,p2 in zip(cipher,cipher[code:]):
+    #     print(p1,p2)
+    print(cipher[4::4])
+    return answer
+    
+
+
+solution("123456789123456789"	,4)
