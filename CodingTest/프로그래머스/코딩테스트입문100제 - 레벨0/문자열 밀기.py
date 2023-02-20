d2e5f68009745282826ef8b53afb2c4bf755e57d
jaewoo <comjayoncloud@gmail.com>
2023-02-20 20:53:11 +0900

    코테 100 : 문자열 밀기

--- CodingTest/프로그래머스/코딩테스트입문100제 - 레벨0/문자열 밀기.py
+++ CodingTest/프로그래머스/코딩테스트입문100제 - 레벨0/문자열 밀기.py
@@ -1,3 +1,43 @@
+import copy
+
 def solution(A, B):
-    answer = 0
-    return answer
+    C=[]
+    result = 0
+    # 배열 길이만큼 밀어보고 if 조건에 맞으면 1 아니면 -1
+    for i in range(len(A)):
+        if A==B:
+            return 0
+        else :
+            print(A[-1])
+            C.append(A[-1])
+            C += A[0:-1]
+            print(C)
+            result +=1
+            if(''.join(C) == B):
+                return result
+            else :
+                A = copy.deepcopy(C)
+                C.clear()
+        
+    return -1
+
+solution("apple","elppa")
+
+
+"""
+다른사람코드1
+    solution=lambda a,b:(b*2).find(a)
+
+다른사람코드2
+    def solution(A, B):
+        #if A == "":
+        #    return 0
+
+        AA = A+A
+        answer = AA.find(B)
+
+        if answer >0:
+            answer = len(A) - answer
+
+        return answer
+"""
