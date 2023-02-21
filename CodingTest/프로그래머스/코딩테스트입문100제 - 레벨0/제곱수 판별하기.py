c44a2ea5b74378f691c538f3ed9f77f6514088a4
jaewoo <comjayoncloud@gmail.com>
2023-02-21 22:16:01 +0900

    코테 100 : 제곱수 판별하기

---
+++ CodingTest/프로그래머스/코딩테스트입문100제 - 레벨0/제곱수 판별하기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    for i in range(1,n+1):
+        if i*i==n:
+            return 1
+    return 2
+
+solution(144)
+
+
+"""
+다른사람코드
+    def solution(n):
+        return 1 if (n ** 0.5).is_integer() else 2
+"""
