b4a6f74fc2ee68c4c82167bdf2ce9d311c689c4e
jaewoo <comjayoncloud@gmail.com>
2023-02-22 20:11:17 +0900

    코테 LV-1 : 문자열 정수로 바꾸기

---
+++ CodingTest/프로그래머스/코딩테스트 - 레벨1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = 0
+    return answer
+
+"""
+다른사람코드
+    def strToInt(str): 
+        result = 0
+        for idx, number in enumerate(str[::-1]):
+            if number == '-':
+                result *= -1
+            else:
+                result += int(number) * (10 ** idx)
+        return result
+
+분석 및 느낀점
+    너무 어렵게 푼거같다. 굳이..?
+"""
