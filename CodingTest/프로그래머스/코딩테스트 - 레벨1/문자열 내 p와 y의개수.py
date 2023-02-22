4f72c778299c062d3eb01778cd8791d41037746f
jaewoo <comjayoncloud@gmail.com>
2023-02-22 18:59:40 +0900

    코테 LV-1 : 문자열 내 p,y개수

---
+++ CodingTest/프로그래머스/코딩테스트 - 레벨1/문자열 내 p와 y의개수.py
@@ -0,0 +1,14 @@
+def solution(s):
+    print(False==False)
+    return print(s.lower().count('p') == s.lower().count('y'))
+
+solution("aaa")
+"""
+1. 기준을 잡자. 소문자로 변환
+2. p,y 있을때
+
+궁금점.
+    왜 p,y둘다 없을때 true가 나오지? 항상 참을 반환??
+    False == False => True 이기때문..
+"""
+
--- 프로그래머스/코딩테스트입문100제/피자나눠먹기2.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    pizza = 1
-    while (pizza*6)%n!=0:
-        pizza +=1
-        print(pizza)
-    return pizza
-
-"""
-"""
