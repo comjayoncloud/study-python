e6f45728690e513119d8a3970996930877a6593a
jaewoo <comjayoncloud@gmail.com>
2023-02-23 18:32:43 +0900

    코테 LV-1 : 직사각형 별뽑기

---
+++ CodingTest/프로그래머스/코딩테스트 - 레벨1/직사각형  별찍기.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().strip().split(' '))
+
+
+for _ in range(1,b+1):
+    print("*"*a)
+
+
+"""
+다른사람코드1
+    a, b = map(int, input().strip().split(' '))
+    answer = ('*'*a +'\n')*b
+    print(answer)
+
+"""
