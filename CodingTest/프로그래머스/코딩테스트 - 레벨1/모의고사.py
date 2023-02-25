b3945ecaba649bce4d77b1ce2e1360884eb66f77
jaewoo <comjayoncloud@gmail.com>
2023-02-25 22:27:58 +0900

    코테 LV-1 : 모의고사

--- CodingTest/프로그래머스/코딩테스트 - 레벨1/모의고사.py
+++ CodingTest/프로그래머스/코딩테스트 - 레벨1/모의고사.py
@@ -1,10 +1,20 @@
 def solution(answers):
-    answer = []
-    a1 = [1, 2, 3, 4, 5] # 1번째 수포자의 답을 넣은 리스트
-    a2 = [2, 1, 2, 3, 2, 4, 2, 5] # 2번째 수포자의 답을 넣은 리스트
-    a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    print(a1[0%5])
-    print(a1[1%5])
-    return answer
+    counts = [0,0,0]
+    winner = []
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5,]
+    
+    for i in range(len(answers)):
+        if answers[i] == s1[(i%5)]:
+            counts[0] += 1
+        if answers[i] == s2[(i%8)]:
+            counts[1] += 1
+        if answers[i] == s3[(i%10)]:
+            counts[2] += 1
+            
+    for i in range(3):
+        if counts[i] == max(counts):
+            winner.append(i+1)
 
-solution([1,2,3,4,5])
+    return winner
