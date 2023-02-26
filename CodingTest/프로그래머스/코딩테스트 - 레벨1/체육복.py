855eeaf93884f5c41e96192b0479d381132075a8
jaewoo <comjayoncloud@gmail.com>
2023-02-26 21:03:18 +0900

    코테 LV-1 : 체육복

--- CodingTest/프로그래머스/코딩테스트 - 레벨1/체육복.py
+++ CodingTest/프로그래머스/코딩테스트 - 레벨1/체육복.py
@@ -1,29 +1,34 @@
-def solution(n, lost, reserve):
+
+def solution(n, lost, reserve): 
+    answer = 0 
+    #중복제거
+    reserve_del = set(reserve)-set(lost) # 1,5
     
-    a = len(lost)
-    b = len(reserve)
+    #중복제거
+    lost_del = set(lost)-set(reserve) 
+    print(reserve_del)
+    for i in reserve_del: 
+        if i-1 in lost_del: 
+            lost_del.remove(i-1) 
+            
+        elif i+1 in lost_del: 
+            lost_del.remove(i+1) 
+            
+    answer = n - len(lost_del)
+    
+    return answer
 
-    if a == b :
-        return n
-    elif a < b:
-        return n
-    elif a > b:
-        print("1")
 
-
-    return 0
-
-
-solution(5,[2,4],[1,3,5])
+solution(5,[2,4],[1,2,5])
 
 """
-전체학생수 n
-도난 당한 학생들 배열 lost
-여벌의 체육복 가져온 학생들 reverse
+전체학생수 n -> 1,2,3,4,5
+도난 당한 학생들 배열 lost -> 2,4
+여벌의 체육복 가져온 학생들 reverse -> 1,3,5
 
-1. 도난 당한수 == 더가져온수 그냥 n 리턴
-2. 도난 당한수 < 더 가져온수 그냥 n 리턴
+\ 여벌 체육복을 가져온 학생이 도난 당했을수도 있다. 이경우엔 줄수가없다 lost 2 == reverse 2면 빌려줄수가없다.
 
-3. 도난 당한수가 더 많은경우
-    3-1 
+
+
+3,4,5,8,9,12,24
 """
